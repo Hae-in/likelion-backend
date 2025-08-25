52c116430460172ee949f4b7d3e326feb1b93ba3
haein <93hilee@naver.com>
2025-08-26 04:15:14 +0900

    add day06
    - 메소드 오버라이딩

---
+++ src/day06/override/Cat.java
@@ -0,0 +1,18 @@
+package day06.override;
+
+public class Cat extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("야옹~");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("고양이가 살금살금 걷습니다.");
+    }
+
+    // Cat만의 고유 메소드
+    public void scratch() {
+        System.out.println("할퀴기!");
+    }
+}
