2359d38a0ac0e00f2ecbbab02cccd6e5566cdb75
haein <93hilee@naver.com>
2025-09-09 23:09:40 +0900

    project: 은행계좌관리시스템
    - 메인 함수 구현
      - 프로그램 실행 흐름을 제어

--- src/project/bank/Main.java
+++ src/project/bank/Main.java
@@ -3,5 +3,7 @@ package project.bank;
 public class Main {
     public static void main(String[] args) {
         BankSystem bankSystem = new BankSystem();
+        bankSystem.loadDataFromFile();
+        bankSystem.displayMenu();
     }
 }
