5b03903e4a7775a82b671a2f8d03d971d02615dc
haein <93hilee@naver.com>
2025-09-16 13:17:02 +0900

    feat: insert

--- src/main/java/com/likeliom/demo/repository/ProductDAOImpl.java
+++ src/main/java/com/likeliom/demo/repository/ProductDAOImpl.java
@@ -1,13 +1,38 @@
 package com.likeliom.demo.repository;
 
 import com.likeliom.demo.dto.ProductDTO;
+import lion.jdbc.dept.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 public class ProductDAOImpl implements ProductDAO {
 
     @Override
-    public boolean insertProduct(ProductDTO product) {
-        return false;
+    public boolean insertProduct(ProductDTO product) {boolean result = false;
+
+        String sql = "insert into products(name, price, reg_date) values(?,?, now())";
+
+        try(
+                // 접속
+                Connection conn = DBUtil.getConnection();
+                // 쿼리 작성
+                PreparedStatement ps = conn.prepareStatement(sql);
+        ) {
+            ps.setString(1, product.getNsme());
+            ps.setInt(2, product.getPrice());
+
+            // 실행
+            int count = ps.executeUpdate();
+
+            if(count > 0) {
+                result = true;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
     }
 
     @Override
--- src/main/java/com/likeliom/demo/service/ProductTest.java
+++ src/main/java/com/likeliom/demo/service/ProductTest.java
@@ -1,7 +1,13 @@
 package com.likeliom.demo.service;
 
+import com.likeliom.demo.dto.ProductDTO;
+import com.likeliom.demo.repository.ProductDAO;
+import com.likeliom.demo.repository.ProductDAOImpl;
+
 public class ProductTest {
     public static void main(String[] args) {
-
+        ProductDAO productDAO = new ProductDAOImpl();
+        boolean resultFlag = productDAO.insertProduct(new ProductDTO("book", 20000));
+        System.out.println(resultFlag);
     }
 }
