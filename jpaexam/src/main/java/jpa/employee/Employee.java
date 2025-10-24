f5cf03f614c2b1106dd8feeb6c435f14bd46f883
haein <93hilee@naver.com>
2025-10-24 12:17:59 +0900

    feat: employee

---
+++ jpaexam/src/main/java/jpa/employee/Employee.java
@@ -0,0 +1,35 @@
+package jpa.employee;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "employees_projects",
+            joinColumns = @JoinColumn(name = "employee_id"),
+            inverseJoinColumns = @JoinColumn(name = "project_id")
+    )
+    private Set<Project> projects = new HashSet<>();
+
+
+    public Employee(String name) {
+        this.name = name;
+    }
+}
---
+++ jpaexam/src/main/java/jpa/employee/EmployeeMain.java
@@ -0,0 +1,48 @@
+package jpa.employee;
+
+import jakarta.persistence.EntityManager;
+import jpa.JPAUtil;
+
+public class EmployeeMain {
+    public static void main(String[] args) {
+        create();
+    }
+
+    private static void create() {
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+        em.getTransaction().begin();
+        try {
+            Employee employee = new Employee("hong");
+
+            // Project project = new Project("lion project");
+            Project project = em.find(Project.class, 2L);
+
+            employee.getProjects().add(project);
+            project.getEmployees().add(employee);
+
+            em.persist(employee);
+            em.persist(project);
+
+
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+
+    }
+
+    private static void update() {
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+        em.getTransaction().begin();
+        try {
+
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+
+    }
+}
