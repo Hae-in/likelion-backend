7668be827dc522a1e52e521b6e67fcb69bcefc28
haein <93hilee@naver.com>
2025-10-24 12:17:23 +0900

    feat: book

---
+++ jpaexam/src/main/java/jpa/book/Author.java
@@ -0,0 +1,29 @@
+package jpa.book;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(mappedBy = "author", fetch = FetchType.EAGER)
+    private List<Book> books = new ArrayList<>();
+}
---
+++ jpaexam/src/main/java/jpa/book/BookMain.java
@@ -0,0 +1,31 @@
+package jpa.book;
+
+import jakarta.persistence.EntityManager;
+import jpa.JPAUtil;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BookMain {
+    private static void find() {
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+
+        try {
+            Author author = em.find(Author.class, 1L);
+            log.info("Author Name: {}", author.getName());
+            System.out.println("Author Name: " + author.getName());
+
+            log.info("Book List size : {}", author.getBooks().size());
+
+            // Book book = em.find(Book.class, 1L);
+            // log.info("Book title: {}", book.getTitle());
+
+            // log.info("Book author: {}", book.getAuthor());
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        find();
+    }
+}
