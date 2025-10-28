31ce113e8a2732749f947994efa571968740c2d0
haein <93hilee@naver.com>
2025-10-28 15:03:59 +0900

    feat: HelloController

---
+++ restexam/src/main/java/org/example/restexam/controller/HelloController.java
@@ -0,0 +1,35 @@
+package org.example.restexam.controller;
+
+import org.example.restexam.domain.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi rest controller";
+    }
+
+    @GetMapping("/user")
+    public User user(){
+        return new User(1L, "goldbb", "goldbb@gmail.com");
+    }
+
+    // GET 요청 처리
+    @GetMapping("/greeting")
+    public Map<String, String> greet(
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name) {
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, " + name + "!");
+        response.put("timestamp", LocalDateTime.now().toString());
+        return response;
+    }
+
+}
