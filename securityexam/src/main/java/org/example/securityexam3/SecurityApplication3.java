d8fecd1091de27dca5341ed88b3e35afc909764e
haein <93hilee@naver.com>
2025-11-05 11:07:08 +0900

    feat: role 추가

---
+++ securityexam/src/main/java/org/example/securityexam3/SecurityApplication3.java
@@ -0,0 +1,11 @@
+package org.example.securityexam3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication3 {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication3.class, args);
+    }
+}
---
+++ securityexam/src/main/java/org/example/securityexam3/SecurityConfig.java
@@ -0,0 +1,72 @@
+package org.example.securityexam3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/home", "/img/**", "/static/**").permitAll()
+                        .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")  // hasAnyRole
+                        .requestMatchers("/admin/super").hasRole("SUPERUSER")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated()
+                );
+
+        http
+                .formLogin(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/hello")
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user1 = User.withUsername("user")
+                .password(passwordEncoder.encode("1234"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withUsername("goldbb")
+                .password(passwordEncoder.encode("1234"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        UserDetails user3 = User.withUsername("admin")
+                .password(passwordEncoder.encode("1234"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user4 = User.withUsername("superuser")
+                .password(passwordEncoder.encode("1234"))
+                .roles("SUPERUSER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1,user2,user3,user4);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
